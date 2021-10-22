6c313b61ac64e4afa90af05d5baf336b810b0bb2
Maxwel <maxwelfcoelho@gmail.com>
2021-10-22 10:13:46 -0300

    list all users filtering by user tasks

--- server/controllers/task.ts
+++ server/controllers/task.ts
@@ -3,7 +3,8 @@ import { error } from '../libs/bindError'
 import * as task from '../services/task'
 
 const list =  async (req: Request<any>, res: Response<any>) => {
-    const tasks = await task.list()
+    const userId = req.user.userId
+    const tasks = await task.list(userId)
     res.status(200).json(tasks)
 }
 
@@ -11,7 +12,7 @@ const create = async (req: Request<any>, res: Response<any>) => {
     try {
         const name = req.body.name
         const userId = req.user.userId
-        
+
         await task.create(name, userId)
 
         res.status(200).json({ message: 'Task created sucessfully' })
--- server/services/task.ts
+++ server/services/task.ts
@@ -1,13 +1,13 @@
 import { Task } from "../models/taskModel"
 
-const list = async () => {
-    const tasks = await Task.find()
+const list = async (userId: string) => {
+    const tasks = await Task.find({ user: userId })
     return tasks
 }
 
 const create = async (name: string, userId: string) => {
     if (!name) {
-        throw new Error('Email is required')
+        throw new Error('name is required')
     }
 
     const newTask = new Task({
